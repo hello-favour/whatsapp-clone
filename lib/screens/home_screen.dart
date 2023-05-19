e1f894713d07411ed5552b36526aa84b6529a2d3
Favour <hello.williamsfav@gmail.com>
2023-05-19 21:53:14 +0100

    build tabbar screens

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/screens/calls_screen.dart';
+import 'package:whatsapp_clone/screens/chats_screen.dart';
+import 'package:whatsapp_clone/screens/status_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -7,9 +10,54 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController tabController;
+
+  @override
+  void initState() {
+    tabController = TabController(length: 3, vsync: this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
+        title: const Text("WhatsApp"),
+        elevation: 0.5,
+        actions: [
+          const Icon(Icons.search),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.more_vert),
+          ),
+        ],
+        bottom: TabBar(
+          indicatorColor: Colors.white,
+          controller: tabController,
+          tabs: const [
+            Tab(text: "CHATS"),
+            Tab(text: "STATUS"),
+            Tab(text: "CALLS"),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: tabController,
+        children: const [
+          CallsScreen(),
+          ChatsScreen(),
+          StatusScreen(),
+        ],
+      ),
+    );
   }
 }
