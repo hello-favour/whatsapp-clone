e7d70c438b9ebb0b4513680b68fa84709d104fe9
Favour <hello.williamsfav@gmail.com>
2023-05-21 07:59:13 +0100

    done building call screen

---
+++ lib/models/call_list.dart
@@ -0,0 +1,64 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class Call {
+  final String name;
+  final String time;
+  final String callImage;
+  final Icon status;
+  final Icon type;
+  Call({
+    required this.callImage,
+    required this.name,
+    required this.time,
+    required this.status,
+    required this.type,
+  });
+}
+
+List<Call> callList = [
+  Call(
+    callImage:
+        "https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg",
+    name: "Busket",
+    time: "May 17, 9:20 Pm",
+    status: const Icon(
+      Icons.call_received,
+      color: Colors.red,
+    ),
+    type: const Icon(Icons.phone),
+  ),
+  Call(
+    callImage:
+        "https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg",
+    name: "Paul",
+    time: "Yesterday 17, 9:27 Am",
+    status: const Icon(
+      Icons.call_received,
+      color: Colors.green,
+    ),
+    type: const Icon(Icons.videocam),
+  ),
+  Call(
+    callImage:
+        "https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg",
+    name: "Gospel Clement",
+    time: "Yesterday 21, 12:33 PM",
+    status: const Icon(
+      Icons.call_received,
+      color: Colors.green,
+    ),
+    type: const Icon(Icons.phone),
+  ),
+  Call(
+    callImage:
+        "https://bestinau.com.au/wp-content/uploads/2018/04/Trends-in-technology-that-are-changing-the-world-1280x720.jpg",
+    name: "Thankidrumz",
+    time: "May 17, 3:33 PM",
+    status: const Icon(
+      Icons.call_received,
+      color: Colors.red,
+    ),
+    type: const Icon(Icons.videocam),
+  ),
+];
--- lib/screens/calls_screen.dart
+++ lib/screens/calls_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/constants/image_path.dart';
+import 'package:whatsapp_clone/models/call_list.dart';
+import 'package:whatsapp_clone/theme/appcolors.dart';
 
 class CallsScreen extends StatefulWidget {
   const CallsScreen({super.key});
@@ -10,8 +13,51 @@ class CallsScreen extends StatefulWidget {
 class _CallsScreenState extends State<CallsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("CALLS SCREEN"),
+    return ListView.builder(
+      itemCount: callList.length,
+      itemBuilder: (context, index) {
+        final call = callList[index];
+        return Column(
+          children: [
+            const Divider(height: 5, thickness: 1.2),
+            ListTile(
+              title: Text(
+                call.name,
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+              leading: CircleAvatar(
+                radius: 25,
+                backgroundImage: NetworkImage(
+                  ImagePath.profileImage,
+                ),
+              ),
+              subtitle: Container(
+                padding: const EdgeInsets.only(top: 5),
+                child: Row(
+                  children: [
+                    call.status,
+                    Text(
+                      call.time,
+                      style: const TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  call.type.icon,
+                  color: AppColors.primaryColor,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
